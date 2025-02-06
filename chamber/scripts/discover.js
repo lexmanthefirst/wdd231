e0b6ce5b2029549528941788535039a5bc85f9c2
Okhitoya Alex <lexmanthefirst@gmail.com>
2025-02-06 12:16:52 -0500

    added smooth zoom to images

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -9,7 +9,8 @@ places.forEach(function (place) {
         <h2>${place.name}</h2>
         <p>${place.description}</p>
         <address>${place.address}</address>
-        <img src="images/${place.photo[0]}" alt="${place.photo[1]}" loading="${place.photo[2]}">
+        <div class="image-container"><img src="images/${place.photo[0]}" alt="${place.photo[1]}" loading="${place.photo[2]}"></div>
+        
         `;
   placeWrapper.appendChild(placeCard);
 });
